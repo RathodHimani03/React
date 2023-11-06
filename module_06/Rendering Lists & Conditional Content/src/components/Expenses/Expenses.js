a80b8b163068f542266dab59344462d35453ffd0
pp-3 <himani.r@prominentpixel.com>
2023-11-06 19:06:12 +0530

    Working on rendering List & Conditional content

---
+++ module_06/Rendering Lists & Conditional Content/src/components/Expenses/Expenses.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import ExpenseItem from './ExpenseItem';
+import Card from '../UI/Card';
+import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
+
+
+function Expenses(props) {
+
+  const [filteredYear, setFilteredYear] = useState('2020');
+  
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+    console.log(selectedYear)
+   
+  };
+
+  const filteredExpenses = props.items.filter(expense =>{
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
+
+  
+
+
+  return (
+
+    <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+      <ExpensesList items={filteredExpenses}/>
+      {/* {filteredExpenses.length ===0 &&<p>No expenses found.</p>}
+      {filteredExpenses.length >0 &&
+        filteredExpenses.map((expense) => (
+          
+          <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}/>
+        ))
+      } */}
+    </Card>
+  );
+}
+
+export default Expenses;
