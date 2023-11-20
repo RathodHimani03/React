7d1113f783bd56d1a01f115d6178d0345f76d73a
pp-3 <himani.r@prominentpixel.com>
2023-11-20 19:24:11 +0530

    Adding http & forms to the Food Order App

---
+++ module_18/Adding Http & Forms To The Food Order App/src/Components/Meals/AvailableMeals.js
@@ -0,0 +1,80 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
+import { useEffect, useState } from 'react';
+
+const AvailableMeals = () => {
+
+  const [isLoading, setIsLoading] = useState(true)
+  const [meals, setmeals] = useState([]);
+  const [httpError, setHttpError] = useState();
+
+
+  useEffect(() => {
+
+    const fetchmeals = async () => {
+      const response = await fetch('https://http-request-c043d-default-rtdb.firebaseio.com/meals.json');
+
+      if (!response.ok) {
+        throw new Error('Something went wrong!')
+      }
+
+      const data = await response.json()
+
+      const loadedMeals = []
+      for (let key in data) {
+        loadedMeals.push({
+          id: key,
+          name: data[key].name,
+          describe: data[key].description,
+          price: data[key].price
+        })
+      }
+
+      setmeals(loadedMeals)
+
+    }
+    fetchmeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message)
+
+    });
+    setIsLoading(false)
+  }, [])
+
+  if (isLoading) {
+    return (
+      <section>
+        <p className={classes.MealsLoading}>Loading......</p>
+      </section>
+    );
+  }
+  if (httpError) {
+    return (
+      <section>
+        <p className={classes.ErrorMessage}>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
