8e6469aae2549710f63df53ab634ef7b4aaae4b6
pp-3 <himani.r@prominentpixel.com>
2023-11-21 17:26:44 +0530

    worked on redux & redux-toolkit

--- module_19/Redux Implementation/src/App.js
+++ module_19/Redux Implementation/src/App.js
@@ -1,9 +1,21 @@
 import Counter from './components/Counter';
-
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
+import Header from './components/Header';
+import React from 'react';
+import {  useSelector } from 'react-redux';
 
 function App() {
+
+const isAuth = useSelector(state=>state.auth.isAuthenticated)
+
   return (
+    <React.Fragment>
+      <Header/>
+     {!isAuth && <Auth/>}
+     {isAuth && <UserProfile/>}
     <Counter />
+    </React.Fragment>
   );
 }
 
--- module_19/Redux Implementation/src/components/Header.js
+++ module_19/Redux Implementation/src/components/Header.js
@@ -1,10 +1,22 @@
 import classes from './Header.module.css';
+import { useSelector } from 'react-redux';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { authActions } from '../store/auth-slice';
+
 
 const Header = () => {
+
+  const dispatch = useDispatch();
+  const isAuth = useSelector(state=>state.auth.isAuthenticated);
+ 
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
-      <nav>
+      {
+        isAuth  &&
+        <React.Fragment>
+          <nav>
         <ul>
           <li>
             <a href='/'>My Products</a>
@@ -13,10 +25,12 @@ const Header = () => {
             <a href='/'>My Sales</a>
           </li>
           <li>
-            <button>Logout</button>
+            <button onClick={()=>{dispatch(authActions.logout())}}>Logout</button>
           </li>
         </ul>
       </nav>
+        </React.Fragment>
+      }
     </header>
   );
 };
